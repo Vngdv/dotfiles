17e9b12125cbe873dfc241e6c79d2fea39597caa
Andre Rica <andre.ricamendez@fit.fichtner.de>
2025-05-05 10:04:09 +0200

    add sample for create link

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,5 @@
 win-stow.ps1 -Stowdir C:\Users\andre\ -Sourcedir C:\Users\andre\.dotfiles\ -Stow nvim
 
 [System.Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME',$HOME + '/.config', 'User')
+
+# New-Item -Path C:\Users\USER\.config\nvim -ItemType SymbolicLink -Value C:\Users\USER\.dotfiles\nvim\
